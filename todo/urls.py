cf8f8e9114c6cab48a03f72e61359979317bcce3
hiroki shimizu <hiroki71027923@icloud.com>
2024-11-16 18:18:37 +0900

    DeleteViewとUpdateViewを使用して、削除と更新の機能を追加。それぞれのボタンから画面へ遷移できるようにurlを設定

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail, TodoCreate
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
 
 urlpatterns = [
     path('list/', TodoList.as_view(), name='list'),
     # DetailViewは任意のデータを一つ取得して表示するという機能を持つため、primary_keyによって任意のデータを指定する
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
-    path('create/', TodoCreate.as_view(), name='create')
+    path('create/', TodoCreate.as_view(), name='create'),
+    # DeleteViewはどのデータを削除するのか一意に識別できる必要があるため、primary_keyによって任意のデータを指定する
+    path('delete/<int:pk>', TodoDelete.as_view(), name='delete'),
+    # UpdateViewはどのデータを更新するのか一意に識別できる必要があるため、primary_keyによって任意のデータを指定する
+    path('update/<int:pk>', TodoUpdate.as_view(), name='update')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import TodoModel
 from django.urls import reverse_lazy
 
@@ -17,6 +17,19 @@ class TodoDetail(DetailView):
 # CreateViewを継承
 class TodoCreate(CreateView):
   template_name = 'create.html'
+  model = TodoModel
+  fields = ('title', 'memo', 'priority', 'duedate')
+  success_url = reverse_lazy('list')
+
+# DeleteViewを継承
+class TodoDelete(DeleteView):
+  template_name = 'delete.html'
+  model = TodoModel
+  success_url = reverse_lazy('list')
+
+
+class TodoUpdate(UpdateView):
+  template_name = 'update.html'
   model = TodoModel
   fields = ('title', 'memo', 'priority', 'duedate')
   success_url = reverse_lazy('list')
