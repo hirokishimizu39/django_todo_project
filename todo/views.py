1f369188a0b778854ac2ece7c43e62528ed803d7
hiroki shimizu <hiroki71027923@icloud.com>
2024-11-16 17:44:53 +0900

    CreateViewを利用し、データ作成（csrfも出てきた）

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 # ListViewを継承
@@ -12,3 +13,10 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
   template_name = 'detail.html'
   model = TodoModel
+
+# CreateViewを継承
+class TodoCreate(CreateView):
+  template_name = 'create.html'
+  model = TodoModel
+  fields = ('title', 'memo', 'priority', 'duedate')
+  success_url = reverse_lazy('list')
